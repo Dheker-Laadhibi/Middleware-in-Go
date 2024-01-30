50fcba5b599bccfa415afcb36a92c5d4f5b3368d
dheker <dhekerlaadhibii@gmail.com>
2024-01-30 22:44:14 +0100

    creating midd file

---
+++ midd.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+
+	 "fmt"
+      "time"
+
+	"net/http"
+
+
+
+
+)
+ func main(){
+	fmt.Println("starting")
+
+//router=>middleware=>Application Handler 
+// Application Handler render response to the client 
+
+
+
+
+
+ }
